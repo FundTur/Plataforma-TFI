772aa791005f5016a9ac865145625e202dc6361b
JuanPBedoya <juanbedoya843@gmail.com>
2023-04-13 19:17:39 -0500

    Integracion con mysql docker

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,13 +1,37 @@
 import { DataSource } from "typeorm";
 
+//Begin entities
+  import { Beneficio } from "./model/Beneficio";
+  import { Categoria } from "./model/Categoria";
+  import { Ciudad } from "./model/Ciudad";
+  import { Convocatoria } from "./model/Convocatoria";
+  import { Observacion } from "./model/Observacion";
+  import { Pago } from "./model/Pago";
+  import { Pais } from "./model/Pais";
+  import { Plan } from "./model/Plan";
+  import { Rol } from "./model/Rol";
+  import { Usuario } from "./model/Usuario";
+//End entities
+
 export const AppSource = new DataSource({
   type: "mysql",
-  host: "192.168.100.70",
-  username: "root",
-  password: "root",
-  database: "development",
+  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   port: 3306,
-  entities: ["./src/model/*.ts"],
+  entities: [
+    Beneficio,
+    Categoria,
+    Ciudad,
+    Convocatoria,
+    Observacion,
+    Pago,
+    Pais,
+    Plan,
+    Rol,
+    Usuario
+  ],
   synchronize: true,
   logging: true,
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,17 @@ import app from "./App";
 import { AppSource } from "./db";
 
 async function main() {
-  // Inicializamos el orm
-  await AppSource.initialize();
+  try {
+    // Inicializamos el orm
+    await AppSource.initialize();
 
-  // Start the server
-  app.listen(3000, () => {
-    console.log("Server running in http://localhost:3000");
-  });
+    // Start the server
+    app.listen(3000, () => {
+      console.log("Server running in http://localhost:3000");
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 main();
